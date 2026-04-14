159d96ff3b4e901e83346b3c283cc274cce78916
MaMuMaMow <166376767+MaMuMaMow@users.noreply.github.com>
2026-04-14 09:13:21 +0700

    waitGroup

--- TCP-scanner.go
+++ TCP-scanner.go
@@ -21,7 +21,7 @@ func slow_scanning(){
 func faster_scanning(){ //faster but not accurate
 	for i := 1; i <= 1024; i++ {
 		go func(j int){
-			webAddress := fmt.Sprintf("scanme.nmap.org:%d", i)
+			webAddress := fmt.Sprintf("scanme.nmap.org:%d", j)
 		con,err := net.Dial("tcp",webAddress)
 		if err != nil {
 			return
@@ -32,11 +32,25 @@ func faster_scanning(){ //faster but not accurate
 	}
 }
 
-func faster_accurate_scanning(){
-	
+func waitGroup_scanning(){
+	var wg sync.WaitGroup
+	for i := 1; i <= 65535; i++ {
+		wg.Add(1)
+		go func(j int){
+			defer wg.Done()
+			webAddress := fmt.Sprintf("127.0.0.1:%d", j)
+			conn,err := net.Dial("tcp",webAddress)
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("port %d is open\n",j)
+		}(i)
+	}
+	wg.Wait()
 }
 
 func main(){
-	faster_scanning()
+	waitGroup_scanning()
 }
 
